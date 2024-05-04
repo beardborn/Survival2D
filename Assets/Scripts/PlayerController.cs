c94fc36991b2723d8721757786de019b291e4098
Christopher Hancock <beardborngaming@gmail.com>
2024-05-04 21:17:51 +0100

    Added keyboard and controller support

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,30 @@ public class PlayerController : MonoBehaviour
     [Header("Elements")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private MobileJoystick playerJoystick;
+    private Vector3 keyboardMovement = new Vector3();
+
 
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float keyboardMoveSpeed;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        keyboardMovement.x = Input.GetAxisRaw("Horizontal");
+        keyboardMovement.y = Input.GetAxisRaw("Vertical");
+    }
+
     private void FixedUpdate()
     {
-        rb.velocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
+        if (playerJoystick.GetMoveVector() != Vector3.zero)
+        {
+            rb.velocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
+        }
+        else { rb.velocity = keyboardMovement.normalized * keyboardMoveSpeed * Time.deltaTime; }
     }
 }
