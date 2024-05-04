e68dc1f6c24d7fd4963784b6826408256e3ffac9
Christopher Hancock <beardborngaming@gmail.com>
2024-05-04 20:29:17 +0100

    Add joystick movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private MobileJoystick playerJoystick;
+    [SerializeField] private float moveSpeed;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.right;
     }
 
     private void Update()
     {
-        
+        rb.velocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
     }
 }
